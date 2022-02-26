df4954507320fce44280f00c5b426e579da99980
GaneshMane16 <100488742+GaneshMane16@users.noreply.github.com>
2022-02-27 02:35:26 +0530

    product mdel and memory acess

---
+++ MyShop/Myshop.Core/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myshop.Core.models
+{
+    class Product
+    {
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string  Id { get; set; }
+        public string Name { get; set; }
+        public string Descrition { get; set; }
+        public decimal Price { get; set; }
+        public string  Category { get; set; }
+        public string     Image { get; set; }
+
+
+        Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+
+        }
+    }
+}
